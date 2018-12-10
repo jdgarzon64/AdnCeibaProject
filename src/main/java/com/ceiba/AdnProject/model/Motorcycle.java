a3c5e1931c8da3b02ad0cae04a68411603e7c198
jdgarzon64 <jdgarzon64@misena.edu.co>
2018-12-10 00:18:19 -0500

    coverage

--- src/main/java/com/ceiba/AdnProject/model/Motorcycle.java
+++ src/main/java/com/ceiba/AdnProject/model/Motorcycle.java
@@ -4,11 +4,6 @@ import com.ceiba.AdnProject.model.util.VehicleTypeEnum;
 
 public class Motorcycle extends Vehicle  {
 
-	public Motorcycle() {
-		super();
-		this.vehicleType = new VehicleType(VehicleTypeEnum.MOTORCYCLE.name());
-	}
-	
 	public Motorcycle(String licence,String engine) {
 		super();
 		this.licenceNumber= licence;
