4b61af6b4558f51655c69679203a2c4c67c01119
jdgarzon64 <jdgarzon64@misena.edu.co>
2018-12-12 16:37:09 -0500

    updated smells

---
+++ src/main/java/com/ceiba/AdnProject/constants/ValidMessageResponse.java
@@ -0,0 +1,9 @@
+package com.ceiba.adnproject.constants;
+
+public class ValidMessageResponse {
+
+	public static final String VEHICLE_REGISTERED= "This Vehicle Was Registered Correctly";
+	
+	public static final String PAYMENT_REGISTERED= "This Payment Was Registered Correctly";
+	
+}
--- src/main/java/com/ceiba/AdnProject/factory/VehicleFactoryImpl.java
+++ src/main/java/com/ceiba/AdnProject/factory/VehicleFactoryImpl.java
@@ -1,31 +1,33 @@
 package com.ceiba.adnproject.factory;
 
 import org.springframework.stereotype.Service;
-
+import com.ceiba.adnproject.constants.InvalidMessageResponse;
 import com.ceiba.adnproject.dto.InputDTO;
 import com.ceiba.adnproject.exception.ParkingException;
 import com.ceiba.adnproject.model.Car;
 import com.ceiba.adnproject.model.Motorcycle;
 import com.ceiba.adnproject.model.Vehicle;
+import com.ceiba.adnproject.model.util.VehicleTypeEnum;
+
 @Service
 public class VehicleFactoryImpl implements IVehicleFactory{
-public static final String LICENCE_INVALID=	"Licence Invalid";
+
 	@Override
 	public Vehicle createVehicle(InputDTO inputDTO) throws ParkingException{
-		if(!verifyLicence(inputDTO.getEngine()))  throw new ParkingException (LICENCE_INVALID);
-		if (inputDTO.getType().equalsIgnoreCase("CAR")) {
+		if(!verifyEngine(inputDTO.getEngine()))  throw new ParkingException (InvalidMessageResponse.ENGINE_INVALID);
+		if (inputDTO.getType().equalsIgnoreCase(VehicleTypeEnum.CAR.name())) {
 			return new Car(inputDTO.getLicence(),inputDTO.getEngine());
 		} else {
 			return new Motorcycle(inputDTO.getLicence(), inputDTO.getEngine());
 		}
 	}
 	
-	public boolean verifyLicence (String value) throws ParkingException {
+	public boolean verifyEngine (String value) throws ParkingException {
 		try {	
 		Integer.parseInt(value);
 			return true;
 		}catch(Exception e) {
-			 throw new ParkingException (LICENCE_INVALID);
+			 throw new ParkingException (InvalidMessageResponse.ENGINE_INVALID);
 		}
 	}
 }
